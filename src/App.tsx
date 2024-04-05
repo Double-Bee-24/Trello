1d91d7f4c7d8acd4b6b17dcf5131f046bad12cae
Bohdan Bilokin <bodya24102001@gmail.com>
2024-04-05 20:20:21 +0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Board } from './pages/Board/Board';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>React app main page</div>,
+  },
+  {
+    path: '/board',
+    element: <Board />,
+  },
+]);
 
 function App(): JSX.Element {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Board/Board.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Board(): JSX.Element {
+  return <div>Hello board!</div>;
+}
